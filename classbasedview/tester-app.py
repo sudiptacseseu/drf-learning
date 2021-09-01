72ddff1dcd71700eda2fbea191c3ea36c84fd290
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2021-09-01 20:50:47 +0600

    Simple CRUD api with class based view done

---
+++ classbasedview/tester-app.py
@@ -0,0 +1,56 @@
+import json
+import requests
+
+
+URL = "http://localhost:8000/student-api/"
+
+
+def get_data(id=None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    r = requests.get(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+def post_data():
+    data = {
+        'name': 'Antar',
+        'roll': 104,
+        'city': 'Barisal'
+    }
+    json_data = json.dumps(data)
+    r = requests.post(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+def update_data():
+    data = {
+        'id': 5,
+        'name': 'Samia',
+        'roll': 105,
+        'city': 'Chittagong'
+    }
+    json_data = json.dumps(data)
+    r = requests.put(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+def delete_data():
+    data = {
+        'id': 4,
+    }
+    json_data = json.dumps(data)
+    r = requests.delete(url=URL, data=json_data)
+    data = r.json()
+    print(data)
+
+
+get_data()
+# post_data()
+# update_data()
+# delete_data()
