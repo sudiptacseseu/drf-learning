f7760a1b62b946e567a702603386140b66474af9
Sudipta Karmakar <sudiptacse.seu@gmail.com>
2021-09-10 22:12:33 +0600

    Hyperlinked model serializer added on modelviewset project

--- modelviewset/modelviewsetapi/serializers.py
+++ modelviewset/modelviewsetapi/serializers.py
@@ -7,3 +7,10 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'name', 'roll', 'city']
+
+# Hyperlinked model serializer gives default field named url
+# class StudentSerializer(serializers.HyperlinkedModelSerializer):
+#
+#     class Meta:
+#         model = Student
+#         fields = ['id', 'url', 'name', 'roll', 'city']
